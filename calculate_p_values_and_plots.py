5b050b03a4a1074bd3c85838672eab7e6baba400
KaueTND <kaue.unicamp2011@hotmail.com>
2022-07-27 23:27:13 -0600

    New files

--- calculate_p_values_and_plots.py
+++ calculate_p_values_and_plots.py
@@ -12,25 +12,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-# #regiões
-# #53	Right-Hippocampus
-# #54	Right-Amygdala
-# #49	Right-Thalamus-Proper*
-# #2035	ctx-rh-insula
-# #
-# #17	Left-Hippocampus
-# #18	Left-Amygdala
-# #10	Left-Thalamus-Proper
-# #1035	ctx-lh-insula
-# #
-# #
-# #
-
+##### PLEASE UPDATE YOUR BASE_DIR HERE
+base_dir = 'your/directory/'
+base_dir = 'C:/Users/kaueu/Desktop/new/'
 chosen_stage_1 = 'HN'
-chosen_stage_2 = 'MCI'
+chosen_stage_2 = 'AD'
 
 def generate_values(regs,path_base,path_out,chosen_stage_1,chosen_stage_2):
-    lista_descritores = np.load('C:/Users/kaueu/Desktop/All/SIBGRAPI_2022_Murilo/lista_descritores.npy')
+    lista_descritores = np.load('lista_descritores.npy')
     super_vec_feature = np.zeros((len(regs),61))
     mean_values_master = np.zeros((len(regs),61))
     for idx,reg in enumerate(regs):
@@ -65,14 +54,14 @@ def generate_values(regs,path_base,path_out,chosen_stage_1,chosen_stage_2):
     return [super_vec_feature,mean_values_master]
 
 
-path_base = 'C:/Users/kaueu/Desktop/All/SIPAIM_ValParaiso_Chile/Matrices/Matriz_'
+path_base = base_dir+'Matrices/Matriz_'
 
-regs = np.load('C:/Users/kaueu/Desktop/All/SIPAIM_ValParaiso_Chile/list_regions.npy')
+regs = np.load(base_dir+'list_regions.npy')
 
-patients = np.load('C:/Users/kaueu/Desktop/All/SIPAIM_ValParaiso_Chile/patients.npy')
-patients = [x.replace('\n','') for x in patients]
+#patients = np.load(base_dir+'patients.npy')
+#patients = [x.replace('\n','') for x in patients]
     
-list_regions_organized = pd.read_csv('C:/Users/kaueu/Desktop/All/SIPAIM_ValParaiso_Chile/list_regions_organized.txt',sep = ',')
+list_regions_organized = pd.read_csv(base_dir+'list_regions_organized.txt',sep = ',')
 
 
 
@@ -88,11 +77,9 @@ types = {'NC':[0,100],
 
 case = chosen_stage_1+'v'+chosen_stage_2
 
-path_out = 'C:/Users/kaueu/Desktop/All/SIPAIM_ValParaiso_Chile/'+case
+path_out = base_dir+case
 
 case = chosen_stage_1+ ' vs '+chosen_stage_2
-
-#[super_vec_feature,mean_values] = generate_values(regs,path_base,path_out,chosen_stage_1,chosen_stage_2)
 
 
 # ##########################
@@ -111,10 +98,9 @@ df_mean = pd.merge(list_regions_organized,df_mean,how='left',on='regioes_id')
 super_vec_feature = df.to_numpy()[:,2:].astype('float')
 mean_values = df_mean.to_numpy()[:,2:].astype('float')
 regs=df['regioes_id']
-#nome=df['regioes_nome']
-#lista_descritores = df.columns[0:-1]
+
 test = super_vec_feature<0.05
-lista_descritores = np.load('C:/Users/kaueu/Desktop/All/SIBGRAPI_2022_Murilo/lista_descritores.npy')
+lista_descritores = np.load(base_dir+'lista_descritores.npy')
 
 color_image_left = np.array([1,2,1,2,3,3,1,2,1,2,
                     1,2,1,2,1,2,1,2,3,3,
@@ -130,10 +116,6 @@ color_image_left = np.array([color_image_left]*61)[:,:].T
 
 
 
-
-#plt.figure()
-#sns.heatmap(mean_values>0)
-################### PLOT THE DESCRIPTORS OR REGIONS
 ax=0
 
 summ = np.sum(test,axis=ax)
@@ -145,167 +127,17 @@ else:
 df2 = pd.DataFrame(summ)
 df2[0] = pd.to_numeric(df2[0])
 
-#import seaborn as sns
-#import matplotlib.pyplot as plt
-
 df2 = df2.sort_values(by=0)
 
-#print(np.arange(len(df2[1])))
-#plt.figure(figsize=(14,5))
-#plt.title('P-values')
-#plt.plot(np.arange(len(df2[1])),df2[0])
-#plt.xticks(np.arange(len(df2[1])),df2[1],rotation=90)
-
-####################################### PLOT THE P_VALUE MATRIX
-
-# plt.figure(figsize=(15,15))
-# plt.title('p-values')
-
-# #super_vec_feature = np.nan_to_num(super_vec_feature)
-
-# #print(super_vec_feature)
-
-# ax = sns.heatmap(super_vec_feature.astype('float32'),cmap='bwr')
-
-# font = {'family' : 'normal',
-#         'weight' : 'regular',
-#         'size'   : 22}
-
-# plt.rc('font', **font)
-
-# #nome = nome.to_numpy()
-# lista_descritores = lista_descritores.to_numpy()
-# plt.xticks(np.arange(len(lista_descritores)),lista_descritores,rotation=45,ha='right')
-# plt.yticks(np.arange(len(regs)),regs,rotation=45)
-
-# for n, label in enumerate(ax.xaxis.get_ticklabels()):
-#     if n % 5 != 0:
-#         label.set_visible(False)
-
-# for n, label in enumerate(ax.yaxis.get_ticklabels()):
-#     if n % 5 != 0:
-#         label.set_visible(False)
-
-# plt.savefig(path_out+'pvalues_part.png',bbox_inches='tight')
-
-
-########################################################## PLOT THE UNCORRECTED VALUES
-
-
-#plt.figure(figsize=(12,15))
-#plt.title('uncorrected p<0.05')
-#ax = sns.heatmap(test,cbar=None,cmap='gray')
-
-
-#font = {'family' : 'normal',
-#        'weight' : 'regular',
-#        'size'   : 22}
-
-#plt.rc('font', **font)
-
-#plt.xticks(np.arange(len(lista_descritores)),lista_descritores,rotation=45,ha='right')
-#plt.yticks(np.arange(len(regs)),regs,rotation=45)
-
-#for n, label in enumerate(ax.xaxis.get_ticklabels()):
-#    if n % 5 != 0:
-#        label.set_visible(False)
-
-#for n, label in enumerate(ax.yaxis.get_ticklabels()):
-#    if n % 5 != 0:
-#        label.set_visible(False)
-
-#plt.savefig(path_out+'p-values_uncorr_part.png',bbox_inches='tight')
-
-
-############################################################ PLOT THE CORRECTED VALUES
-
-
-#plt.figure(figsize=(12,15))
 
 fdrcorrected = list()
 fdr_values = list()
-super_vec_feature = super_vec_feature
 for x in super_vec_feature:
     fdrcorrected.append(fdrcorrection(x,alpha=0.05)[0])
     fdr_values.append(fdrcorrection(x,alpha=0.05)[1])
 fdrcorrected = np.array(fdrcorrected)
 fdr_values = np.array(fdr_values)
 fdrcorrected_df = pd.DataFrame(data=fdrcorrected,columns=lista_descritores.ravel(),index=regs.ravel())
-
-#show_x = np.sum(fdrcorrected,axis=0)
-#show_y = np.sum(fdrcorrected,axis=1)
-
-#font = {'family' : 'normal',
-#        'weight' : 'regular',
-#        'size'   : 22}
-
-#plt.rc('font', **font)
-
-#plt.title('corrected p<0.05')
-
-#plt.imshow(fdrcorrected,cmap='gray')
-#ax = sns.heatmap(fdrcorrected_df,cbar=None,cmap='gray')
-
-#every_nth=20
-#plt.xticks(np.arange(len(lista_descritores)),lista_descritores,rotation=45,ha='right')
-#plt.yticks(np.arange(len(regs)),regs,rotation=45)
-
-#for n, label in enumerate(ax.xaxis.get_ticklabels()):
-#    if show_x[n] == 0:
-#        label.set_visible(False)
-
-#for n, label in enumerate(ax.yaxis.get_ticklabels()):
-#    if show_y[n] == 0:
-#        label.set_visible(False)
-
-#plt.savefig(path_out+'p-values_last_part.png',bbox_inches='tight')
-
-
-
-
-#### ALL THE PLOTS 
-
-#####################################################################################PLOT DIFFERENCE 
-
-fig = plt.figure(figsize=(12,15))
-plt.title(case+' - Difference '+chosen_stage_1+' (Red) - '+chosen_stage_2+' (Blue)')
-mean_values_n = mean_values*1./np.max(mean_values, axis=0)
-ax = sns.heatmap(mean_values_n.astype('float32'),cmap='bwr',cbar=None)
-font = {'family' : 'normal',
-        'weight' : 'regular',
-        'size'   : 22}
-
-plt.rc('font', **font)
-plt.xlabel('Texture Features')
-plt.ylabel('Brain Regions')
-#
-#set the labels
-xticks = [ 11, 32, 48, 61]
-xticks_minor = [ 23, 40, 56]
-yticks = [1.8,7.1,8.8,11.1,12.8,14.6,16.9,19.5,30.1,47.1,58.1,72.1,83.5]#[3, 17.9, 34, 42,74]
-yticks_minor = [6,8,10,12,14,16,18,23,47,55,69,83]#yticks_minor = [18, 33, 38, 70]
-ylbls=['Ven','T','C','P','U','H','A','CC','Frontal','Occipital','Temporal','Parietal','In']#ylbls = [ 'Left dGM', 'Right dGM ', 'CC','Left cortical area','Right cortical area']
-xlbls = [ 'GLCM', 'GLRLM', 'GLSZM','NGTDM']
-#ax = fig.add_axes( [.05, .1, .9, .85 ] )
-#set ticks
-ax.set_xticks( xticks )
-ax.set_xticklabels( xlbls )
-ax.set_xticks( xticks_minor, minor=True )
-ax.set_yticks( yticks )
-ax.set_yticklabels( ylbls ,rotation=90)
-ax.set_yticks( yticks_minor, minor=True )
-
-#set the grid 
-#ax.set_xticklabels( xlbls, minor=True )
-ax.set_xlim( 0, 61 )
-plt.grid(visible=True,color='k',which='minor')
-ax.tick_params( axis='x', which='minor', direction='out', length=30 )
-ax.tick_params( axis='x', which='major', bottom='off', top='off' )
-ax.tick_params( axis='y', which='minor', direction='out', length=30 )
-ax.tick_params( axis='y', which='major', bottom='off', top='off' )
-
-plt.savefig(path_out+'_difference.pdf',bbox_inches='tight')
-
 
 ###################################################################################################PVALUES
 
@@ -518,62 +350,3 @@ plt.savefig(path_out+'_descriptor_pval_corrected.pdf',bbox_inches='tight')
 
 
 #########################################################################################DIFF
-
-fig = plt.figure(figsize=(12,15))
-
-#show_x = np.sum(fdrcorrected,axis=0)
-#show_y = np.sum(fdrcorrected,axis=1)
-
-#show_x = show_x * np.arange(len(show_x))
-#show_x = show_x[show_x >0]
-#show_y = (show_y>0) * np.arange(len(show_y))
-#show_y = show_y[show_y >0]
-
-font = {'family' : 'normal',
-        'weight' : 'regular',
-        'size'   : 22}
-
-plt.rc('font', **font)
-
-plt.title('Difference TS Higher (Blue) v NC Higher (Red)')
-#(-1 = TS) (+1 = NC)   -1(TS Higher) -  1(NC Higher)
-thresh = ((mean_values.astype('float32')>0)*2)-1
-
-#plt.imshow(fdrcorrected,cmap='gray')
-ax = sns.heatmap(fdrcorrected*thresh,cmap='bwr',vmin=-1,vmax=1,cbar=None)
-
-plt.xlabel('Texture Features')
-plt.ylabel('Brain Regions')
-#
-#set the labels
-xticks = [ 11, 32, 48, 61]
-xticks_minor = [ 23, 40, 56]
-yticks = [3, 17.9, 34, 42,74]
-yticks_minor = [18, 33, 38, 70]
-ylbls = [ 'Left dGM', 'Right dGM ', 'CC','Left cortical area','Right cortical area']
-xlbls = [ 'GLCM', 'GLRLM', 'GLSZM','NGTDM']
-#ax = fig.add_axes( [.05, .1, .9, .85 ] )
-#set ticks
-ax.set_xticks( xticks )
-ax.set_xticklabels( xlbls )
-ax.set_xticks( xticks_minor, minor=True )
-ax.set_yticks( yticks )
-ax.set_yticklabels( ylbls ,rotation=90)
-ax.set_yticks( yticks_minor, minor=True )
-
-
-
-#set the grid 
-#ax.set_xticklabels( xlbls, minor=True )
-ax.set_xlim( 0, 61 )
-plt.grid(visible=True,color='#ffffff',which='minor')
-
-ax.tick_params( axis='x', which='minor', direction='out', length=30 )
-ax.tick_params( axis='x', which='major', bottom='off', top='off' )
-
-ax.tick_params( axis='y', which='minor', direction='out', length=30 )
-ax.tick_params( axis='y', which='major', bottom='off', top='off' )
-
-
-
-plt.savefig(case+'_difference.pdf',bbox_inches='tight')
